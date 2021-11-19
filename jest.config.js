79f94388f1c05567f38653a3ca5a828493d0bd6f
@Elias <eliasonofre2013@gmail.com>
2021-11-19 10:37:48 -0500

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverageFrom: '<rootDir>/src/**/*.ts',
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }

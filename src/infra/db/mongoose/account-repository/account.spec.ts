f627bd569df946b69673295a790c3625655ed749
@Elias <eliasonofre2013@gmail.com>
2021-12-01 19:10:40 -0500

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongoose/account-repository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017/cleanArqui')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('Accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
